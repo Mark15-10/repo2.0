b9d299a524beb951951721a1ba57b28f513fb589
Mario Rojas <mario.rojaset12d1@gmail.com>
2025-05-06 21:16:24 -0300

    calculadora,etc

--- codigo.js
+++ codigo.js
@@ -1,11 +1,25 @@
 
-function mifuncion(){
+function mostrarDato(){
     var entrada = document.getElementById('insertar');
-    var texto = document.getElementById('primer-parrafo');
-    texto.innerText += entrada.value
-
+    var texto = document.getElementById('resultado');
+    texto.innerText += 'El numero es: + \n'+entrada.value
+    
 };
 
+function vaciar(){
+    var texto = document.getElementById('resultado');
+    texto.innerText = '';
+}
 
-document.getElementById('boton').onclick = mifuncion;
+function sumarNumeros(){
+    var texto = document.getElementById('resultado')
+    var nuevo_numero = document.getElementById('insertar');
+    resultado += parseInt( nuevo_numero.value); 
+    texto.innerText += 'La suma es:  ' + resultado + '\n';
+    
+}
+
+
+var resultado= 0;
+// document.getElementById('boton').onclick = mifuncion;
 
